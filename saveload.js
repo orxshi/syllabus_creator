aaaaddba31df3e78636565bb4dfe3270e60b2220
orxshi <orxshi@gmail.com>
2025-10-06 00:20:55 +0300

    >

--- saveload.js
+++ saveload.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-    $("#save").click(function(e){
-        var jsonData = {};
 
+    // ===== SAVE FUNCTION =====
+    $("#save").click(function(e){
+        e.preventDefault(); // prevent default action
+
+        var jsonData = {};
         var formData = $("#myform").serializeArray();
 
+        // Convert form data into JSON, handling multiple values
         $.each(formData, function() {
             if (jsonData[this.name]) {
-                if (!jsonData[this.name].push) {
+                if (!Array.isArray(jsonData[this.name])) {
                     jsonData[this.name] = [jsonData[this.name]];
                 }
                 jsonData[this.name].push(this.value || '');
@@ -14,54 +18,75 @@ $(document).ready(function() {
                 jsonData[this.name] = this.value || '';
             }
         });
-        $.ajax(
-        {
-            url : "encode.php",
-                type: "POST",
-                data : jsonData,
-                success: function(data)
-                {
-                    window.location.href = 'save.php';
-                }
+
+        // AJAX to save JSON
+        $.ajax({
+            url: "encode.php",
+            type: "POST",
+            data: jsonData,
+            success: function(data) {
+                window.location.href = 'save.php';
+            }
         });
     });
-});
 
-$(document).ready(function() {
+    // ===== LOAD FUNCTION =====
     $("#load").click(function(e){
+        e.preventDefault();
+
         var fileDialog = $('<input type="file">');
         fileDialog.click();
-        fileDialog.on("change",onFileSelected);
+
+        fileDialog.on("change", onFileSelected);
     });
+
+    // ===== FILE READ & FORM POPULATION =====
+    var onFileSelected = function(e){
+        let file = $(this)[0].files[0];
+        let reader = new FileReader();
+
+        reader.readAsText(file);
+
+        reader.onload = function() {
+            let obj = reader.result;
+            let data = JSON.parse(obj);
+
+            for (var key in data) {
+
+                // Handle checkboxes (arrays)
+                if (Array.isArray(data[key])) {
+                    data[key].forEach(function(val) {
+                        $('input[name="'+key+'"][value="'+val+'"]').prop('checked', true);
+                    });
+                } else {
+                    // Handle text, textarea, select
+                    let el = $('#' + key);
+
+                    if (el.is(':checkbox')) {
+                        el.prop('checked', data[key] == el.val());
+                    } else if (el.is('select')) {
+                        el.val(data[key]);
+                    } else {
+                        el.val(data[key]);
+                    }
+                }
+            }
+        };
+    };
+
 });
 
-var onFileSelected = function(e){
+// ===== RESET FUNCTION =====
+function resetAll() {
+    // Reset all text inputs
+    $("input[type='text']").val('');
 
-    let fn = $(this)[0].files[0];
+    // Reset all textareas
+    $("textarea").val('');
 
-    let reader = new FileReader();
+    // Reset all checkboxes
+    $("input[type='checkbox']").prop('checked', false);
 
-    reader.readAsText(fn);
-
-    reader.onload = function() {
-        let obj = reader.result;
-        let data = JSON.parse(obj);
-        for (var i in data) {
-            $('#'+i).val(data[i]);
-        }
-    };
-};
-
-function resetAll()
-{
-    var elements = document.getElementsByTagName("input");
-    for (var ii=0; ii < elements.length; ii++) {
-        if (elements[ii].type == "text") {
-            elements[ii].value = "";
-        }
-    }
-    var elements = document.getElementsByTagName("textarea");
-    for (var ii=0; ii < elements.length; ii++) {
-        elements[ii].value = "";
-    }
+    // Reset all selects
+    $("select").prop('selectedIndex', 0);
 }
